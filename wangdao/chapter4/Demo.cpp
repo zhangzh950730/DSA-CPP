5732a6e5c7f0b090c568c0b243a8b6043b98a904
zhangzh <zhangzh@gisway.com.cn>
2021-11-25 18:00:42 +0800

    4.1 串的定义和实现

---
+++ wangdao/chapter4/Demo.cpp
@@ -0,0 +1,4 @@
+//
+// Created by 张之豪 on 2021/11/25.
+//
+#include "String.h"
---
+++ wangdao/chapter4/String.h
@@ -0,0 +1,78 @@
+//
+// Created by 张之豪 on 2021/11/25.
+//
+#include <cstdlib>
+#include <cstdio>
+
+#define MAX_LEN  255
+
+typedef struct {
+    char ch[MAX_LEN];
+    int length;
+} SString;
+
+typedef struct {
+    char *ch;
+    int length;
+} HString, String;
+
+
+int StrLength(String S) {
+    int i = 0;
+    while (*(S.ch + i++));
+    return --i;
+}
+
+void SubString(String &sub, String S, int start, int end) {
+    char *p = sub.ch = (char *) malloc(sizeof(char));
+    for (int i = 0; i < end; ++i) {
+        *p = *(S.ch + i + start);
+        p = p + 1;
+    }
+    sub.length = end;
+}
+
+int StrCompare(String S, String T) {
+    if (S.length != T.length) return false;
+    char *p = S.ch, *q = T.ch;
+    for (int i = 0; i < S.length; ++i) {
+        if (*p != *q) return 0;
+        else {
+            p = (p + 1);
+            q = (q + 1);
+        }
+    }
+    return 1;
+}
+
+int Index(String S, String T) {
+    int i = 1, n = StrLength(S), m = StrLength(T);
+    while (i < n - m + 1) {
+        String sub;
+        SubString(sub, S, i, m);
+        if (StrCompare(sub, T) != 0) ++i;
+        else return i;
+    }
+    return 0;
+}
+
+
+int main() {
+    char ch[] = {'a', 'b', 'c', 'd'};
+    String S = {ch, 4};
+    int length = StrLength(S);
+    printf("%d\n", length);
+
+
+    char ch1[] = {'c', 'd'};
+    String T = {ch1, 2};
+
+    int compare = StrCompare(S, T);
+    printf("compare: %d\n", compare);
+
+    String sub;
+    SubString(sub, S, 2, 2);
+
+    compare = StrCompare(sub, T);
+    printf("compare: %d\n", compare);
+}
